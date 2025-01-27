61ac64e62d162a55d1958b697dccebc0b596bdb1
zulfio <zulfikar1ahmad@gmail.com>
2025-01-27 17:52:12 +0700

    increase sqlite connection limit

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -18,12 +18,22 @@ type database struct {
 }
 
 func NewDatabase(databasePath string) *database {
-	client, err := gorm.Open(sqlite.Open(databasePath), &gorm.Config{
+	client, err := gorm.Open(sqlite.Open(databasePath+"?_busy_timeout=5000"), &gorm.Config{
 		Logger: gormLogger.Default.LogMode(gormLogger.Silent),
 	})
 	if err != nil {
 		logger.Fatal("Error creating gorm database. ", err)
 	}
+
+	// Get the underlying *sql.DB object
+	sqlDB, err := client.DB()
+	if err != nil {
+		logger.Fatal("Error getting underlying database connection. ", err)
+	}
+
+	// Set connection pool settings
+	sqlDB.SetMaxOpenConns(1) // This is crucial for SQLite to prevent "database is locked" errors
+	sqlDB.SetMaxIdleConns(1)
 
 	return &database{
 		client: client,
